b67c7a40a1284f42a1cbda0792047078b830d415
vtechguys <vtechguys@gmail.com>
2019-10-17 00:10:47 +0530

    sendResponse

--- utils/responser/index.js
+++ utils/responser/index.js
@@ -1,10 +1,8 @@
 
 'use strict'
 
-const config = require('../../config');
-const responseTypes = config.responseTypes;
 
-function defaultResonseObj(msg, data = null, errors = null){
+function defaultResonseObj(msg, data = null, errors = null) {
     return {
         message: msg,
         data: data,
@@ -13,30 +11,30 @@ function defaultResonseObj(msg, data = null, errors = null){
 }
 
 const responser = {
-    badRequest(response, message = "Bad request.", data = null, errors = null){
+    badRequest(response, message = "Bad request.", data = null, errors = null) {
         const responseObj = defaultResonseObj(message, data, errors);
-        
+
         responseObj.code = 400;
 
         response.json(responseObj);
 
     },
-    serverError(response, message = "Some Error Occured.", data = null, errors = null){
+    serverError(response, message = "Some Error Occured.", data = null, errors = null) {
         const responseObj = defaultResonseObj(message, data, errors);
         responseObj.code = 500;
         response.json(responseObj);
     },
-    notFound(response, message = "Not found.", data = null, errors = null){
+    notFound(response, message = "Not found.", data = null, errors = null) {
         const responseObj = defaultResonseObj(message, data, errors);
         responseObj.code = 404;
         response.json(responseObj);
     },
-    unauthorized(response, message = "Unauthorized.", data = null, errors = null){
+    unauthorized(response, message = "Unauthorized.", data = null, errors = null) {
         const responseObj = defaultResonseObj(message, data, errors);
         responseObj.code = 401;
         response.json(responseObj);
     },
-    success(response, message = "Success.", data = null, errors = null){
+    success(response, message = "Success.", data = null, errors = null) {
         const responseObj = defaultResonseObj(message, data, errors);
         responseObj.code = 200;
         response.json(responseObj);
